c17968855d4d932ce2987f47ffa6494b1bd72aa5
patricktiamson <pattiam@udel.edu>
2024-04-04 16:37:34 -0400

    swapping tiles functionality

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -3,20 +3,13 @@ import FpsText from "../objects/fpsText";
 
 export default class MainScene extends Phaser.Scene {
     fpsText: FpsText;
-<<<<<<< HEAD
-    private boardSize: number = 8;
-    private imageSize: number = 64;
-    private tiles: Phaser.GameObjects.Sprite[] = [];
-    private tilePositions: Phaser.GameObjects.Sprite[][] = [];
-    private draggingTile: Phaser.GameObjects.Sprite | null = null;
-
-=======
-    private boardSize: number = 3;
+    private boardSize: number = 4;
     private imageSize: number = 130;
-    private tiles: Phaser.GameObjects.Sprite[] = [];
     private score: number = 0;
     private tileTypes: string[];
->>>>>>> 43c4843b4ae50f4b431e2ab3684d0aed7cf84de6
+    private tilePositions: Phaser.GameObjects.Sprite[][] = []; //tile sprites (edit name)
+    private selectedTile: Phaser.GameObjects.Sprite | null = null;
+
     constructor() {
         super({ key: "MainScene" });
     }
@@ -27,15 +20,14 @@ export default class MainScene extends Phaser.Scene {
         const screenHeight = this.game.scale.height;
         this.tileTypes = ["tile1", "tile2", "tile3", "tile4"];
 
-        this.tiles.forEach((tile) => {
-            tile.setInteractive();
-            tile.on("pointerdown", this.startDragging.bind(this));
-            tile.on("pointerup", this.stopDragging.bind(this));
-            tile.on("pointerover", this.updateDragging.bind(this));
-        });
+        this.generateTiledBoard(screenWidth, screenHeight);
+
+        // Add input event listeners
+        this.input.keyboard?.on("keydown", this.handleKeydown, this);
     }
 
     update() {}
+
     private generateTiledBoard(screenWidth: number, screenHeight: number) {
         const tileSize = this.imageSize;
         const boardWidth = this.boardSize * tileSize;
@@ -53,93 +45,103 @@ export default class MainScene extends Phaser.Scene {
                 const tileTypeKey = Phaser.Math.RND.pick(this.tileTypes);
 
                 const tile = this.add.sprite(tileX, tileY, tileTypeKey);
+                tile.setInteractive();
+                tile.on("pointerdown", this.selectTile.bind(this, tile));
                 tile.setOrigin(0, 0);
                 tile.setScale(1);
 
-                this.tiles.push(tile);
                 this.tilePositions[row][col] = tile;
             }
         }
     }
 
-    private startDragging(
-        pointer: Phaser.Input.Pointer,
-        gameObject: Phaser.GameObjects.Sprite
-    ) {
-        console.log("Tile Clicked");
-        this.draggingTile = gameObject;
-        this.input.setTopOnly(true);
+    private selectTile(tile: Phaser.GameObjects.Sprite) {
+        if (this.selectedTile) {
+            this.selectedTile.setTint(0xffffff); // Reset tint of previously selected tile
+        }
+        this.selectedTile = tile;
+        tile.setTint(0xff0000); // Tint the selected tile red
     }
 
-    private stopDragging() {
-        console.log("Tile Released");
-        this.input.setTopOnly(false);
-        this.draggingTile = null;
-    }
+    private handleKeydown(event: KeyboardEvent) {
+        if (!this.selectedTile) return;
 
-    private updateDragging(
-        pointer: Phaser.Input.Pointer,
-        gameObject: Phaser.GameObjects.Sprite
-    ) {
-        if (!this.draggingTile) return;
-        console.log("Dragging");
-        const dragTileRow =
-            (this.tiles.indexOf(this.draggingTile) / this.boardSize) | 0;
-        const dragTileCol =
-            this.tiles.indexOf(this.draggingTile) % this.boardSize;
-        const targetRow = (this.tiles.indexOf(gameObject) / this.boardSize) | 0;
-        const targetCol = this.tiles.indexOf(gameObject) % this.boardSize;
+        let currentRow = -1;
+        let currentCol = -1;
 
-        if (
-            this.isAdjacentTile(dragTileRow, dragTileCol, targetRow, targetCol)
-        ) {
-            this.swapTiles(dragTileRow, dragTileCol, targetRow, targetCol);
+        // Find the row and column of the selected tile
+        for (let row = 0; row < this.boardSize; row++) {
+            const col = this.tilePositions[row].indexOf(this.selectedTile);
+            if (col !== -1) {
+                currentCol = col;
+                currentRow = row;
+                break;
+            }
+        }
+
+        if (currentRow === -1 || currentCol === -1) {
+            // Selected tile not found in the board, handle this case if needed
+            return;
+        }
+
+        let newRow = currentRow;
+        let newCol = currentCol;
+        console.log(event.key);
+        switch (event.key) {
+            case "ArrowUp":
+            case "w":
+                newRow = Math.max(currentRow - 1, 0);
+                break;
+            case "ArrowDown":
+            case "s":
+                newRow = Math.min(currentRow + 1, this.boardSize - 1);
+                break;
+            case "ArrowLeft":
+            case "a":
+                newCol = Math.max(currentCol - 1, 0);
+                break;
+            case "ArrowRight":
+            case "d":
+                newCol = Math.min(currentCol + 1, this.boardSize - 1);
+                break;
+        }
+
+        if (newRow !== currentRow || newCol !== currentCol) {
+            console.log(newRow);
+            console.log(newCol);
+            this.swapTiles(currentRow, currentCol, newRow, newCol);
         }
     }
 
-    private isAdjacentTile(
-        row1: number,
-        col1: number,
-        row2: number,
-        col2: number
-    ) {
-        return (
-            (Math.abs(row1 - row2) === 1 && col1 === col2) ||
-            (Math.abs(col1 - col2) === 1 && row1 === row2)
-        );
-    }
-
     private swapTiles(row1: number, col1: number, row2: number, col2: number) {
-        console.log("swapping");
-        const tileSize = this.imageSize;
         const tile1 = this.tilePositions[row1][col1];
         const tile2 = this.tilePositions[row2][col2];
-
+        console.log(tile1, tile2);
+        const tweenDuration = 300;
+        //create tile1 tween
         this.tweens.add({
-            targets: [tile1, tile2],
-            x: {
-                getEndValue: () => {
-                    if (this.draggingTile === tile1) {
-                        return tile2.x;
-                    } else {
-                        return tile1.x;
-                    }
-                },
-            },
-            y: {
-                getEndValue: () => {
-                    if (this.draggingTile === tile1) {
-                        return tile2.y;
-                    } else {
-                        return tile1.y;
-                    }
-                },
-            },
-            duration: 200,
+            targets: tile1,
+            x: tile2.x,
+            y: tile2.y,
+            duration: tweenDuration,
             onComplete: () => {
-                this.tilePositions[row1][col1] = tile2;
                 this.tilePositions[row2][col2] = tile1;
+                this.selectedTile?.setTint(0xffffff);
+                this.selectedTile = null; // Deselect the tile after swapping
+                console.log("tween1 complete");
             },
         });
+        //create tile2 tween
+        this.tweens.add({
+            targets: tile2,
+            x: tile1.x,
+            y: tile1.y,
+            duration: tweenDuration,
+            onComplete: () => {
+                this.tilePositions[row1][col1] = tile2;
+                console.log("tween2 complete");
+            },
+        });
+        console.log("adding tween");
     }
 }
