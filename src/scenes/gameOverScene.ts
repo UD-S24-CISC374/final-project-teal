38706c82693bd49145b89cc9b2553552d10e8b26
Angelo Ramos <angelora@udel.edu>
2024-04-19 18:24:09 -0400

    Refactor GameOverScene UI elements for better readability and consistency

--- src/scenes/gameOverScene.ts
+++ src/scenes/gameOverScene.ts
@@ -5,25 +5,45 @@ export default class GameOverScene extends Phaser.Scene {
     }
 
     create() {
+        const width: number = this.game.config.width as number;
+        const height: number = this.game.config.height as number;
         this.add
-            .text(500, 300, "Game Over", { fontSize: "32px", color: "#000000" })
-            .setOrigin(0.5);
-        const retryButton = this.add
-            .text(400, 350, "retryButton", {
-                fontSize: "32px",
+            .text(width * 0.5, height * 0.3, "Game Over", {
+                fontSize: "40px",
+                fontFamily: "Arial",
                 color: "#000000",
             })
-            .setInteractive();
+            .setOrigin(0.5);
+        const retryButton = this.add
+            .text(width * 0.5, height * 0.45, "Retry", {
+                fontSize: "32px",
+                fontFamily: "Arial",
+                color: "#ffffff",
+                backgroundColor: "#4e342e",
+                padding: {
+                    x: 20,
+                    y: 10,
+                },
+            })
+            .setInteractive()
+            .setOrigin(0.5);
         retryButton.on("pointerdown", () => {
             this.scene.start("MainGameScene");
         });
 
         const menuButton = this.add
-            .text(400, 400, "menuButton", {
+            .text(width * 0.5, height * 0.55, "Menu", {
                 fontSize: "32px",
-                color: "#000000",
+                fontFamily: "Arial",
+                color: "#ffffff",
+                backgroundColor: "#4e342e",
+                padding: {
+                    x: 20,
+                    y: 10,
+                },
             })
-            .setInteractive();
+            .setInteractive()
+            .setOrigin(0.5);
         menuButton.on("pointerdown", () => {
             this.scene.start("MenuScene");
         });
